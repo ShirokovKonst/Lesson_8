da838d9ab9a53d3de0de658e39c0bdfaf70dc080
Kostya <ashirokovkonstantin@gmail.com>
2023-07-08 23:26:06 +0500

    Create homework 8

---
+++ Lesson_8_HomeWork_4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Lesson_8_HomeWork_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = Prompt("Введите размер массива: ");
+            int[,,] ar = new int[size,size,size];
+            ar = GetArray(size);
+            PrintArray(ar, size);
+            
+            int Prompt(string msg)
+            {
+                Console.WriteLine(msg);
+                return int.Parse(Console.ReadLine());
+            }
+            void PrintArray(int[,,] arr, int s)
+            {
+                for(int i = 0; i < s; i++)
+                {
+                    for(int j = 0; j < s; j++)
+                    {
+                        for(int x = 0; x < s; x++)
+                        {
+                           Console.Write(arr[i,j,x] + " " + "(" + i + "," + j + "," + x + ")" + " ");
+                        }
+                        Console.Write('\n');
+                    }
+                }
+            }
+            int[,,] GetArray(int s)
+            {
+                int[,,] result = new int[s,s,s];
+                int[] temp = new int[s*s*s];
+                int tmp=0;
+                bool flag=true;
+                Random rnd = new Random();
+                int rw = 0; 
+                for(int i = 0; i < s; i++)
+                {
+                    for(int j = 0; j < s; j++)
+                    {
+                        for(int x = 0; x < s; x++)
+                        {
+                            flag = true;
+                            while(flag == true)
+                            {
+                                flag = false;
+                                tmp = rnd.Next(1,99);
+                                for(int a = 0; a < s*s*s; a++)
+                                {
+                                    if (temp[a] == tmp)
+                                    {
+                                    flag = true;
+                                    }
+                                }
+                            }
+                            temp[rw] = tmp;
+                            rw++;
+                            result[i,j,x] = tmp;
+                        }
+                    }
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ Lesson_8_HomeWork_5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lesson_8_HomeWork_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 5;
+            int[,] snake = FillArray(size);
+            PrintArray(snake, size); 
+            void PrintArray(int[,] arr, int s)
+            {
+                for(int i = 0; i < s; i++)
+                {
+                    for(int j = 0; j < s; j++)
+                    {
+                        Console.Write(arr[i,j] + " ");
+                    }
+                    Console.Write('\n');
+                }
+            }
+            int[,] FillArray(int s)
+            {
+                int[,] result = new int[s,s];
+                int value = 1;
+                int i = 0;
+                int j = 0;
+                while(value <= s*s)
+                {
+                    result[i,j] = value;
+                    if (i <= j+1 && i + j < s - 1)
+                    j++;
+                    else if (i < j && i + j >= s - 1)
+                    i++;
+                    else if (i >= j && i + j > s - 1)
+                    j--;
+                    else 
+                    i--;
+                    value ++;
+                }
+                return result;
+            }
+        }
+    }
+}
